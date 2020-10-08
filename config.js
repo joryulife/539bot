ef179a92239807b176ef7c37948386322a3d035c
joryulife <joryulife@gmail.com>
2020-10-08 15:18:00 +0900

    configfile add

---
+++ config.js
@@ -0,0 +1,20 @@
+//チャンネルID
+const channelId = "1655031634";
+export default channelId;
+//チャンネルシークレット
+const channelSecret = "c63cda167f5859ceda5cde822a5a7b5f";
+//アサーション署名キー
+export default channelSecret;
+const assertionSignatureKey1 = "68d1eade-9789-48c1-a309-60586ce23ace";
+const assertionSignatureKey2 = "b10d3fc6-feed-42f2-9182-88dd328b32ea";
+export default assertionSignatureKey1;
+export default assertionSignatureKey2;
+//ユーザーID
+const userId = "U3aa127f38f35ddee3962757fe0d50eba";
+export default userId;
+//ベーシックID
+const basicId = "@049kvxjn";
+export default basicId;
+//チャンネルアクセストークン
+const channelAccessToken = "dvLM1fbPEISIwrO5AMIga2wktVeR1PHVG/BhETbrKYl6uNp3swME7x8oPnbHJGnQcsGNHev6mKF4SOI52Blj8spZjBkJUN9Q2qTVKiKXfnA67jWJb5LEwONSgHCZ/UQXzljh+CrkrMVyd7zMzjLJXgdB04t89/1O/w1cDnyilFU=";
+export default channelAccessToken;
---
+++ import.js
@@ -0,0 +1,1 @@
+import channelSecret from "./config"
