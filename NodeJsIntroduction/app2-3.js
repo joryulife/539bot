4ea8fc79f8bb21558c0125055b039428e379d2d3
joryulife <joryulife@gmail.com>
2020-11-07 22:09:04 +0900

    Fix app2-3.js - 6. Correspomds lang='ja' by utf-8.

--- NodeJsIntroduction/app2-3.js
+++ NodeJsIntroduction/app2-3.js
@@ -2,13 +2,13 @@ const http = require('http');
 
 var server = http.createServer(
     function(request,response){
-        response.setHeader('Content=Type','text/html');
+        response.setHeader('Content-Type','text/html');
         response.write('<!DOCTYPE html><html lang="ja">');
-        response.write('<head><meta charset="utf8-">');
+        response.write('<head><meta charset="utf-8">');
         response.write('<title>Hello</title></head>');
         response.write('<body><h1>Hello Node.js!</h1>');
         response.write('<p>This is Node.js sample page.</p>');
-        response.write('<p>これは、Node.jsのサンプルページです。</p>','utf-8');
+        response.write('<p>これは、Node.jsのサンプルページです。</p>','utf8');
         response.write('</body></html>');
         response.end();
     }
