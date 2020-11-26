d7c1d79a980a6d0ca423a6a616f8e8bd1e580995
joryulife <joryulife@gmail.com>
2020-11-26 07:41:54 -0500

    sdk install

--- Knock/app.js
+++ Knock/app.js
@@ -11,7 +11,12 @@ var sendMessage = require('./lib/sendMessage');
 var messageTemplate = require('./lib/MessageTemplate');
 var gnavi = require('./lib/gnaviapi');
 //var pgManager = require('./lib/postgresManager');
-const line = require('./lib/lineinfo');
+const lineinfo = require('./lib/lineinfo');
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+  channelAccessToken: lineinfo.config.channelAccessToken
+});
 
 app.set('port',3000);
 app.use(bodyParser.urlencoded({extended: true}));
@@ -32,33 +37,36 @@ app.post('/callback',knock);
 
 function knock (req, res) {
   console.log("in");
+  //console.log(event_data);
+  // リクエストがLINE Platformから送られてきたか確認する
+  /*if (lineinfo.config.validate_signature(req.headers['x-line-signature'], req.body)) {
+    console.log("if return1");
+    return;
+  }*/
   //waterfallの[]内の無名関数の結果がcallbackされて第２引数の関数の結果が変える。
-  /*async.waterfall([
+  async.waterfall([
       function(callback) {
-
-        let event_data = req.body['events'][0];
-        console.log(event_data);
-        // リクエストがLINE Platformから送られてきたか確認する
-        if (!validate_signature(req.headers['x-line-signature'], req.body)) {
-          return;
-        }
+        let event_data = req.body.events[0];
         // テキストか画像が送られてきた場合のみ返事をする
-        if ((event_data['type'] != 'message') || ((event_data['message']['type'] != 'text') && (event_data['message']['type'] != 'image'))) {
+        if ((event_data.type != 'message') || ((event_data.message.type != 'text') && (event_data.message.type != 'image'))) {
+          console.log("if retun 2");
           return;
         }
-
         // ユーザIDを取得する
-        var user_id = event_data['source']['userId'];
-        var message_id = event_data['message']['id'];
+        var user_id = event_data.source.userId;
+        var message_id = event_data.message.id;
 
-        var message_type = event_data['message']['type'];
-        var message_text = event_data['message']['text'];
+        var message_type = event_data.message.type;
+        var message_text = event_data.message.text;
 
-        if (event_data['source']['type'] != 'user') return;
-
-        request.get(getProfileOption(user_id), function(error, response, body) {
+        if (event_data.source.type != 'user') return;
+        
+        request.get(client.getProfile(user_id), function(error, response, body) {
+          console.log(response.statusCode);
           if (!error && response.statusCode == 200) {
-            callback(req, body['displayName'], message_id, message_type, message_text);
+            callback(req, response.displayName, message_id, message_type, message_text);
+          }else{
+            console.log("else");
           }
         });
       },
@@ -67,12 +75,13 @@ function knock (req, res) {
     // 返事を生成する関数
     function(req, displayName, message_id, message_type, message_text) {
       var message = "";
+      console.log("in2");
       message = "hello, " + displayName + "さん";
       sendMessage.send(req, [messageTemplate.textMessage(message_text)]);
       return;
     }
-  );*/
-};
+  );
+}
 
 // 引数に指定した値以下のランダムな数値を取得する
 function getRandomInt(max) {
@@ -83,17 +92,12 @@ function getRandomInt(max) {
 function getProfileOption(user_id) {
   return {
     url: 'https://api.line.me/v2/bot/profile/' + user_id,
-    //proxy: process.env.FIXIE_URL,
+    proxy: process.env.FIXIE_URL,
     json: true,
     headers: {
-      'Authorization': 'Bearer' + line.config.channelAccessToken
+      'Authorization': 'Bearer' + lineifo.config.channelAccessToken
     }
   };
-}
-
-// 署名検証
-function validate_signature(signature, body) {
-  return signature == crypto.createHmac('sha256', line.config.channelSecret).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
 }
 
 app.listen(app.get('port'), ()=> {
